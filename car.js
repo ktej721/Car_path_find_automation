ea96df2d5dc14732c221af92552774fb35dd7ddb
kartik912 <kartik134yadav@gmail.com>
2025-10-16 14:23:45 +0530

    added the case and its moving up and down

---
+++ car.js
@@ -0,0 +1,30 @@
+class Car{
+    constructor(x,y, width, height){
+        this.x = x;
+        this.y = y;
+        this.height = height;
+        this.width = width;
+
+        this.controls = new Controls();
+    }
+
+    // x and y = 0,0 at the top left of the canvas
+    update(){
+        if(this.controls.forward){
+            this.y -= 2;
+        }
+        if(this.controls.reverse){
+            this.y += 2
+        }
+    }
+    draw(ctx) {
+        ctx.beginPath();
+        ctx.rect(
+            this.x - this.width/2,
+            this.y - this.height/2,
+            this.width,
+            this.height
+        );
+        ctx.fill();
+    }
+}
